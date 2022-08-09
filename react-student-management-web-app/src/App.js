23d442425e1ac432f20deb436ec749dbc454c96b
RamBoddapati <ramu.boddapati@gmail.com>
2022-08-09 08:35:22 -0400

    Changes to web app banner

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management Appl
+            Student Management Appl-Ramarao Training Group
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
