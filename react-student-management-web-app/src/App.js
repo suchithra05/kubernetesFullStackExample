5207dbe9c22280f7b1deae2db906a14edf8648bc
Lucky <86590862+Pavankumarnani@users.noreply.github.com>
2022-08-10 12:53:31 +0530

    Update App.js

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management Appl-Ramarao Training Group
+            Student Management App-pavan
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
